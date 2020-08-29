d431b07e0224f5d319f23331daff86b03b22fc89
Chow <chaulea@gmail.com>
2020-08-29 18:24:54 +0700

    api user v1 update script v3

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const config = require('../config/auth.config');
-const db = require('../models')(mongoose);
+const db = require('../models');
 const User = db.user;
 const Role = db.role;
 const jwt = require('jsonwebtoken');
